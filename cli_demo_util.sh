0c674b06419ce2619ad72f04f41656b442bd14ca
Gaurav Ghimire <gaumire@users.noreply.github.com>
2017-05-25 08:20:00 +1000

    Update cli_demo_util.sh
    Added link to the original

--- cli_demo_util.sh
+++ cli_demo_util.sh
@@ -13,6 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+###
+# Original: https://github.com/kubernetes/contrib/blob/master/micro-demos/util.sh
+###
+
 ##############################################################################
 # demo test.sh
 # #!/bin/bash
